767d3fc308c38f78b690d0ad71edc2f664c70fe1
Mehakpreet kaur <mehakpreetgosal@gmail.com>
2026-02-24 16:41:36 +0530

    using dictionaries and doing operations on them

---
+++ dictionary.py
@@ -0,0 +1,19 @@
+i = 0
+while i <=10:
+    print(i)
+    i += 1
+print("displaying numbers from 1 to 10 using for loop")
+for i in range(1,11):
+    print(i)    
+print("displaying even numbers from 1 to 20")
+for i in range(1,21):
+    if i % 2 == 0:
+        print (i)
+print("displaying multiplication table of 5")
+for i in range(1,51):
+    if i % 5 == 0:
+        print(i)
+string = "hello world"
+for ch in string:
+    print(ch)
+    
