c27a76c12412d91c4f5f3e0717e0caf7106d8623
Mehakpreet kaur <mehakpreetgosal@gmail.com>
2026-03-03 19:17:54 +0530

    practicing more NumPy concepts
    in this file, i practiced NumPy concepts like joining, splitting , searching, sorting, and filtering arrays.

--- practice.py
+++ practice.py
@@ -1,45 +1,42 @@
+#numpy practice 
 import numpy as np
-arr = np.array([[1,2,4,3,5],[6,7,8,9,10],[11,12,13,14,15]])
-print(arr)
-
-print(arr.ndim)
-print(arr.shape)
-#creating an array object
-print(type(arr))
-#accessing an array element
-print(arr[1,1])
-#accessing an array using negative indexing
-print(arr[-2,-4])
-#slicing an array
-print(arr[0:2,1:4])
-#negative slicing of an array
-print(arr[-2:,-4:])
-#checking the datatype of an  array
-print(arr.dtype)
-#creating arrays with defined datatypes using dtype argument.
-arr1 = np.array([1,2,3,4], dtype='S')
-print(arr1)
-print(arr1.dtype)
-#converting datatype on existing array using astype() method
-arr1 = arr.astype('i')
-print(arr1)
-print(arr1.dtype)
-#copy of an array using copy() method
-arr2 = arr.copy()
-print(arr2)
-arr2[0,0]= 100
-print(arr)
-#view of an array using view() method
-arr3 = arr.view()
+#joining two arrays via concatenate() function
+arr1 = np.array([1,2,3])
+arr2 = np.array([4,5,6])
+arr3 = np.concatenate((arr1,arr2))
 print(arr3)
-arr3[0,1] = 100
-print(arr)
-#checking if arrays own its data
-print(arr2.base)
-print(arr3.base)
-#resgaping an array using reshape() method
-arr4 = arr.reshape(5,3)
-print(arr4)
-#using unknown dimensionin reshape() method
-arr5 = arr.reshape(5,3,-1)
-print(arr5)
+#joining a two dimensional array via concatenate() function
+arr4 = np.array([[1,2],[3,4]])
+arr5 = np.array([[5,6],[7,8]])
+arr6 = np.concatenate((arr4,arr5),axis=1) #axis=1 means joining the arrays column-wise while axis=0 means joining the arrays row-wise
+print(arr6) 
+#joining the arrays by stack() function
+arr7 = np.stack((arr4,arr5),axis=0)
+print(arr7)
+#splitting an array via array_split() function
+arr8 = np.array_split(arr6,2,axis=0)
+print(arr8) 
+#accesing the splited array
+print(arr8[0])
+print(arr8[1])
+#splitting a 2-d array
+arr9 = np.array_split(arr4,2,axis=0)
+print(arr9)
+#searching an array via where() function
+x = np.where(arr5==7)
+print(x)
+#finding indexes where values are even
+y = np.where(arr5%2==0)
+print(y)
+#using searchsorted() function
+z = np.searchsorted(arr1,1)
+print(z)
+#sorting an array via sort() function
+arr10 = np.array([3,5,7,7,6,9,4,2,1,0,8])
+arr11 = np.sort(arr10)
+print(arr11)
+#filtering an array
+arr12 = np.array([34,66,78,29])
+a = [True, False, True, False]
+arr13 = arr12[a]
+print(arr13)
